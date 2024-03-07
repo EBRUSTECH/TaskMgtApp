f64b26bff0df13d4c037ce1d8c7b4d9aaaeb30a0
Victory Omoyibo <91475569+EBRUSTECH@users.noreply.github.com>
2024-03-07 05:04:43 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Homepage 1</title>
+    <title>Task mgt app</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap"/>
     <link rel="stylesheet" href="/../../../kit-pro.fontawesome.com/releases/v5.13.0/css/pro.min.css"/>
     <link rel="shortcut icon" type="image/png" href="/assets/images/fav.png"/>
